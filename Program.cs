1ef8e03687ad55db5e33431b335bc9a42585d2e3
Alexssandro Willian <alexssandrowilliam43@gmail.com>
2025-01-30 09:44:15 -0300

    Basic CLI structure added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+DisplayWelcomeMessage();
+List<string> commands = [];
+while (true)
+{
+	Console.Write("Type command: ");
+	var input = Console.ReadLine();
+
+	if(string.IsNullOrEmpty(input)) { continue; }
+
+	commands = input.Split(' ').ToList();
+
+	var exit = false;
+
+	switch (commands[0].ToLower())
+	{
+		case "help":
+			break;
+		case "add":
+			break;
+		case "update":
+			break;
+		case "delete":
+			break;
+		case "mark-in-progress":
+			break;
+		case "mark-done":
+			break;
+		case "list":
+			break;
+		case "clear":
+			break;
+		case "exit":
+			exit = true;
+			break;
+		default:
+			Console.WriteLine("Invalid input! Try again.");
+			break;
+	}
+
+	if (exit) { break; }
+}
+
+Console.WriteLine("Thanks for using the program. ( ^o^)´ Bye");
+
+void DisplayWelcomeMessage()
+{
+	Console.WriteLine("Welcome to Task-Tracker-Cli!");
+	Console.WriteLine("Type 'help' to display the set of commands");
+}
